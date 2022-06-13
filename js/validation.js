ab1a56ca37b6f80d93f8cccef3f64407524136fa
Nahuel Gonzalez <nahuelgonzaleznahuelg2014@gmail.com>
2022-06-13 13:49:54 -0300

    prueba de form y mejoras del front

--- js/validation.js
+++ js/validation.js
@@ -1,16 +1,13 @@
-/* Para personalizar formatos de inputs libreria - jQuery Mask Plugin */
 $( document ).ready(function() {
   if (window.history.replaceState) { // verificamos disponibilidad
       window.history.replaceState(null, null, window.location.href);
   }
 });
 function validate(button){
-  //onclick with this in button type="button"
   $(button).prop('disabled', true);
   var validated = true; 
   var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   
-  // Validate inputs and selects
   $('.required').each(function(){
       if($(this).val() == "" || $(this).val() == null){
           $(this).addClass('is-invalid');
@@ -26,12 +23,12 @@ function validate(button){
           $(this).addClass('is-valid');
       }
   });
-  // Validate email format
+
+  // Validar formato email
   $('.required_email').each(function(){
       if(!regex.test($(this).val())){
           $(this).addClass('is-invalid');
 
-          //Label bellow of input with -> style="display: none"
           $(this).closest('div').find('label').addClass('text-danger');
           validated = false;
       } else {
@@ -45,11 +42,9 @@ function validate(button){
    if(validated){
       $('#contactForm').submit();
 
-      // Do something on success validation
       $(button).prop('disabled', false);
 
   } else {
-      // Do something on failed validation
       $(button).prop('disabled', false);
   } 
 }
